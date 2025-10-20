161aa49610d136bcc87eaf1075585a315cd372eb
hsliuping <107213551@qq.com>
2025-10-20 16:56:42 +0800

    修改导入数据。

--- scripts/import_config_and_create_user.py
+++ scripts/import_config_and_create_user.py
@@ -51,9 +51,7 @@ CONFIG_COLLECTIONS = [
     "datasource_groupings",
     "platform_configs",
     "user_configs",
-    "model_catalog",
-    "market_quotes",        # 实时行情数据
-    "stock_basic_info"      # 股票基础信息
+    "model_catalog"
 ]
 
 
